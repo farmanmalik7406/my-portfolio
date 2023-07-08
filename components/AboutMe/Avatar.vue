cc24a76ccc5560dbdaa3c55387e778b369f2ba9d
farman <farmanmalik7406@gmail.com>
2023-07-08 21:30:37 +0530

    initial commit

---
+++ components/AboutMe/Avatar.vue
@@ -0,0 +1,4 @@
+<template>
+    <b-avatar src="https://placekitten.com/300/300" size="6rem"></b-avatar>
+
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  <Hero />
+  <main id="main">
+  <AboutMe />
+  <Service />
+  <Portfolio />
+  <Testimonial />
+  <Blog />
+  <ContactMe />
+</main>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
